b38c5c39bbcd5c87a9c8ee8828222cc39ceadc35
Marcelo Carvalho <marcelolcarvalho@gmail.com>
2021-05-28 18:04:56 -0300

    Delete people

--- backend/src/main/java/com/algaworks/algamoney/api/exceptionhander/AlgamoneyExceptionHandler.java
+++ backend/src/main/java/com/algaworks/algamoney/api/exceptionhander/AlgamoneyExceptionHandler.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +16,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -42,6 +44,18 @@ public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler {
 
 		List<Error> erros = createErrorList(ex.getBindingResult());
 		return super.handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
+	}
+	
+	@ExceptionHandler({EmptyResultDataAccessException.class})
+	public ResponseEntity<Object> handleEmptyResultDataAccessException(EmptyResultDataAccessException ex ,
+			WebRequest request) {
+		
+		String userMessage = messageSource.getMessage("resouce.not-found", null, LocaleContextHolder.getLocale());
+		String devMessage = ex.toString();
+		List<Error> errors = Arrays.asList(new Error(userMessage, devMessage));
+
+		
+		return super.handleExceptionInternal(ex, errors, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
 	}
 
 	private List<Error> createErrorList(BindingResult bindingResult) {
