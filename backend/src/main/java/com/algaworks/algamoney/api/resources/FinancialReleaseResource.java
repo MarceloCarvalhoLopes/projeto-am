06203092010f5e19c07a2af3449dc6f198d20ee9
Marcelo Carvalho <marcelolcarvalho@gmail.com>
2021-06-08 16:09:28 -0300

    Post FinancialRelease

--- backend/src/main/java/com/algaworks/algamoney/api/resources/FinancialReleaseResource.java
+++ backend/src/main/java/com/algaworks/algamoney/api/resources/FinancialReleaseResource.java
@@ -2,13 +2,21 @@ package com.algaworks.algamoney.api.resources;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.algaworks.algamoney.api.events.ResourceCreatedEvent;
 import com.algaworks.algamoney.api.models.FinancialRelease;
 import com.algaworks.algamoney.api.repositories.FinancialReleaseRepository;
 
@@ -18,6 +26,9 @@ public class FinancialReleaseResource {
 
 	@Autowired
 	private FinancialReleaseRepository financialReleaseRepository;
+	
+	@Autowired
+	private ApplicationEventPublisher applicationEventPublisher;
 	
 	@GetMapping
 	public List<FinancialRelease> findAll(){
@@ -31,4 +42,13 @@ public class FinancialReleaseResource {
 				.orElse(ResponseEntity.notFound().build());
 		
 	}
+	
+	@PostMapping
+	public ResponseEntity<FinancialRelease> create (@Valid @RequestBody FinancialRelease financialRelease, HttpServletResponse response ){
+		FinancialRelease savedFinancial = this.financialReleaseRepository.save(financialRelease);
+		applicationEventPublisher.publishEvent(new ResourceCreatedEvent(this, response, savedFinancial.getId()));
+		return ResponseEntity.status(HttpStatus.CREATED).body(savedFinancial);
+		
+	}
+	
 }
