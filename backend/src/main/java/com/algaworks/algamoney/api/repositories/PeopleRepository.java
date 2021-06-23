a95fd4e97955f3602a4d417789deab60aaa07d78
Marcelo Carvalho <marcelolcarvalho@gmail.com>
2021-06-22 21:00:11 -0300

    People search

--- backend/src/main/java/com/algaworks/algamoney/api/repositories/PeopleRepository.java
+++ backend/src/main/java/com/algaworks/algamoney/api/repositories/PeopleRepository.java
@@ -1,9 +1,12 @@
 package com.algaworks.algamoney.api.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.algaworks.algamoney.api.models.People;
 
 public interface PeopleRepository extends JpaRepository<People, Long> {
 	
+	public Page<People> findByName(String name, Pageable pageable);
 }
--- backend/src/main/java/com/algaworks/algamoney/api/resources/PeopleResource.java
+++ backend/src/main/java/com/algaworks/algamoney/api/resources/PeopleResource.java
@@ -7,6 +7,8 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,11 +41,18 @@ public class PeopleResource {
 	@Autowired
 	private ApplicationEventPublisher applicationEventPublisher;
 
+	
 	@GetMapping
 	@PreAuthorize("hasAuthority('ROLE_SEARCH_PEOPLE') and #oauth2.hasScope('read')")
-	public List<People> findAll() {
-		return peopleRepository.findAll();
+	public Page<People> findByName(@RequestParam(required = false, defaultValue = "%") String name, Pageable pageable) {
+		return peopleRepository.findByName(name, pageable);
 	}
+	
+//	@GetMapping
+//	@PreAuthorize("hasAuthority('ROLE_SEARCH_PEOPLE') and #oauth2.hasScope('read')")
+//	public List<People> findAll() {
+//		return peopleRepository.findAll();
+//	}
 
 	@GetMapping("/{id}")
 	@PreAuthorize("hasAuthority('ROLE_SEARCH_PEOPLE') and #oauth2.hasScope('read')")
@@ -83,4 +93,6 @@ public class PeopleResource {
 	public void updatePropertyActive(@PathVariable Long id, @RequestBody Boolean active) {
 		peopleService.updatePropertyActive(id,active);
 	}
+	
+
 }
