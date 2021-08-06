ee73b8c0c142500fec848b7bf7a164ca81f8fb2c
Marcelo Carvalho <marcelolcarvalho@gmail.com>
2021-08-06 08:49:51 -0300

    17.16 - Implementing the service listAll categories

---
+++ algamoney-ui/src/app/categorias/categoria.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoriaService {
+
+  categoriasUrl = 'localhost:8080/categories';
+
+  constructor(private http: HttpClient) { }
+
+  listAll(): Promise<any>{
+
+    const headers = new HttpHeaders()
+      .append('Authorization', 'Basic YWRtaW5AYWxnYW1vbmV5LmNvbTphZG1pbg==');
+
+      return this.http.get(`${this.categoriasUrl}`,{ headers })
+      .toPromise();
+
+  }
+
+}
