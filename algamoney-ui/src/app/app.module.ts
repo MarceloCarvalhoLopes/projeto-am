d109bc628dca9dc0fe31a48a31f918fb321e17fc
Marcelo Carvalho <marcelolcarvalho@gmail.com>
2021-08-13 14:33:01 -0300

    18.13. Refatorando as rotas para usar Routing Module

--- algamoney-ui/src/app/app.module.ts
+++ algamoney-ui/src/app/app.module.ts
@@ -5,31 +5,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { registerLocaleData } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import localePt from '@angular/common/locales/pt';
-import { RouterModule, Routes } from '@angular/router';
-
 
 import { AppComponent } from './app.component';
 
 import { PessoasModule } from './pessoas/pessoas.module';
 import { LancamentosModule } from './lancamentos/lancamentos.module';
-import { PessoasPesquisaComponent } from './pessoas/pessoas-pesquisa/pessoas-pesquisa.component';
-import { LancamentoCadastroComponent } from './lancamentos/lancamento-cadastro/lancamento-cadastro.component';
-import { LancamentosPesquisaComponent } from './lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component';
-import { PaginaNaoEncontradaComponent } from './core/pagina-nao-encontrada.component';
-
+import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
-
-const routes: Routes = [
-
-  {path: '', redirectTo: 'lancamentos', pathMatch: 'full' },
-  {path: 'lancamentos', component: LancamentosPesquisaComponent},
-  {path: 'lancamentos/novo', component: LancamentoCadastroComponent },
-  {path: 'lancamentos/:id', component: LancamentoCadastroComponent },
-  {path: 'pessoas', component: PessoasPesquisaComponent },
-  {path: 'pagina-nao-encontrada', component: PaginaNaoEncontradaComponent },
-  {path: '**', redirectTo :'pagina-nao-encontrada' }
-
-];
 
 registerLocaleData(localePt);
 
@@ -43,14 +25,11 @@ registerLocaleData(localePt);
     BrowserAnimationsModule,
     FormsModule,
     HttpClientModule,
-    RouterModule.forRoot(routes),
 
-
-
+    CoreModule,
     LancamentosModule,
     PessoasModule,
-    CoreModule
-
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
