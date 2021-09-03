4d89b69c4a31209aef8b7fb581cdf58e60f08e10
MarceloCarvalhoLopes <marcelolcarvalho@gmail.com>
2021-09-03 19:05:24 -0300

    fix Login

--- algamoney-ui/src/app/seguranca/login-form/login-form.component.ts
+++ algamoney-ui/src/app/seguranca/login-form/login-form.component.ts
@@ -1,20 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 import { Router } from '@angular/router';
+
 import { ErrorHandlerService } from 'src/app/core/error-handler.service';
 import { AuthService } from './../auth.service';
-import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.css']
 })
-export class LoginFormComponent  {
+export class LoginFormComponent implements OnInit  {
 
   constructor(
     private auth: AuthService,
     private errorHandlerService: ErrorHandlerService,
-    private router: Router
+    private router: Router,
+    private title: Title
     ) { }
+
+  ngOnInit(): void {
+      this.title.setTitle('Login');
+  }
 
   login(user: string, password : string){
     this.auth.login(user,password)
