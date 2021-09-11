1087bfe60bf37b61aedef402475662231171170d
MarceloCarvalhoLopes <marcelolcarvalho@gmail.com>
2021-09-11 18:20:43 -0300

    Creating the bytes for the report

--- backend/src/main/java/com/algaworks/algamoney/api/services/FinancialReleaseService.java
+++ backend/src/main/java/com/algaworks/algamoney/api/services/FinancialReleaseService.java
@@ -1,5 +1,12 @@
 package com.algaworks.algamoney.api.services;
 
+import java.io.InputStream;
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 import java.util.Optional;
 
 import javax.validation.Valid;
@@ -8,11 +15,17 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.algaworks.algamoney.api.dto.FinancialStatisticPerson;
 import com.algaworks.algamoney.api.models.FinancialRelease;
 import com.algaworks.algamoney.api.models.People;
 import com.algaworks.algamoney.api.repositories.FinancialReleaseRepository;
 import com.algaworks.algamoney.api.repositories.PeopleRepository;
 import com.algaworks.algamoney.api.services.exception.PeopleNonExistentOrInactive;
+
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 
 @Service
 public class FinancialReleaseService {
@@ -23,6 +36,23 @@ public class FinancialReleaseService {
 	@Autowired
 	private FinancialReleaseRepository financialReleaseRepository;
 	
+	public byte[] reportByPerson(LocalDate first, LocalDate end) throws Exception{
+		List<FinancialStatisticPerson> data = financialReleaseRepository.byPerson(first, end);
+		
+		Map<String, Object> parameters  = new HashMap<>();
+		parameters.put("DT_INICIO", Date.valueOf(first));
+		parameters.put("DT_FIM", Date.valueOf(end));
+		parameters.put("REPORT_LOCALE", new Locale("pt","BR"));
+		
+		InputStream inputStream = this.getClass().getResourceAsStream(
+				"reports/lauching-by-person.jasper");
+		
+		JasperPrint jasperPrint = JasperFillManager.fillReport(inputStream, parameters, 
+				new JRBeanCollectionDataSource(data));
+		
+		
+		return JasperExportManager.exportReportToPdf(jasperPrint);
+	}
 	
 	
 	public FinancialRelease save(@Valid FinancialRelease financialRelease) {
@@ -51,7 +81,7 @@ public class FinancialReleaseService {
 			peopleOpt = peopleRepository.findById(financialRelease.getPeople().getId());
 		}
 		
-		if(peopleOpt == null || peopleOpt.isEmpty() || peopleOpt.get().isInactive() ) {
+		if(peopleOpt == null || peopleOpt.get().isInactive() ) {
 			throw new PeopleNonExistentOrInactive();
 		}
 	}
